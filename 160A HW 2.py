346f843fde025f46e8920992fb738875dbd2ea82
pgandham21 <55162907+pgandham21@users.noreply.github.com>
2019-10-17 14:11:30 -0700

    Update 160A HW 2.py

--- 160A HW 2.py
+++ 160A HW 2.py
@@ -11,9 +11,9 @@ import numpy
 songs = [1, 2, 3, 4]
 
 #Counter values to be used to see when 3, 2 and 1 unique songs are picked respectively
-count = 0
-twounique = 0
-oneunique = 0
+threeUnique = 0
+twoUnique = 0
+oneUnique = 0
 
 #Simulate professor listening to 3 songs 10,000 times
 for i in range(10000):
@@ -23,16 +23,16 @@ for i in range(10000):
         lst.append(choice)
     #Conditional to see how many unique songs were picked
     if len(set(lst)) == len(lst):
-        count += 1
+        threeUnique += 1
     elif len(set(lst)) == 2:
-        twounique += 1
+        twoUnique += 1
     else:
-        oneunique +=1
+        oneUnique +=1
 
 #Compute the probability that professor listens to 3, 2, 1 unqiue songs respectively
-probThreeUniqueSongs = count/10000
-probTwoUniqueSongs = twounique/10000
-probOneUniqueSongs = oneunique/10000
+probThreeUniqueSongs = threeUnique/10000
+probTwoUniqueSongs = twoUnique/10000
+probOneUniqueSongs = oneUnique/10000
 print("After 10,000 simulations, the probability that after 3 songs are played that your professor listens to three unique songs is %s ." %probThreeUniqueSongs)
 
 #Calculate average number of unique songs that professor listens to when 3 songs are played
